a0cb980014fa07450380cfe1000037477b56c843
Ruan191 <theredsting@gmail.com>
2021-05-02 23:50:34 +0200

    Add project files.

---
+++ NasaImageOfTheDay/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NasaImageOfTheDay.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using NasaImageOfTheDay.Util;
+using System.IO;
+
+namespace NasaImageOfTheDay.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Image image = new Image();
+            dynamic config = JSON.Parsh(JSON.GetLocal(Directory.GetCurrentDirectory() + @"\Data\Config.json"));
+            dynamic nasa = JSON.Parsh(JSON.GetOnline("https://api.nasa.gov/planetary/apod?api_key=" + config.key));
+
+            image.Title = nasa.title;
+            image.Explanation = nasa.explanation;
+            image.Url = nasa.url;
+
+            return View(image);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NasaImageOfTheDay/Util/JSON.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace NasaImageOfTheDay.Util
+{
+    public class JSON
+    {
+        public static string GetLocal(string path) => File.ReadAllText(path);
+
+        public static string GetOnline(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using Stream stream = response.GetResponseStream();
+            using StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+        public static object Parsh(string content) => JsonConvert.DeserializeObject(content);
+    }
+}
